7543266cb1f973ce701cc2f34e50d19c3ff9c4c2
Jason Kuhrt <jasonkuhrt@me.com>
2025-05-13 14:33:07 -0400

    fix: export type syntax for vitest

--- src/exports/index.ts
+++ src/exports/index.ts
@@ -22,5 +22,5 @@ export * from '../zod-aid/index.js'
 // To satisfy TS
 // e.g.
 // The inferred type of 'create' cannot be named without a reference to '@wollybeard/kit/node_modules/.pnpm/type-fest@4.41.0/node_modules/type-fest'. This is likely not portable. A type annotation is necessary.
-export { type PackageJson } from 'type-fest'
+export type { PackageJson } from 'type-fest'
 export * from 'zx'
