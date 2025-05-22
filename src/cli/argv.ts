82d90f9bd112b8695a7eff1620fa4ae53e273dbf
Jason Kuhrt <jasonkuhrt@me.com>
2025-05-22 18:51:16 -0400

    fix(cli): argv can be only one element

--- src/cli/argv.ts
+++ src/cli/argv.ts
@@ -3,24 +3,31 @@ import { Str } from '../str/index.js'
 
 // Process argv
 
-export type ProcessArgv = [string, string, ...string[]]
+/**
+ * Usually there is a seconed element too, the script that was executed, but not always.
+ * For example in NodeJS Repl it would be missing.
+ */
+export type ProcessArgv = [string, ...string[]]
 
 export const isProcessArgvLoose = (value: unknown): value is ProcessArgv => {
-  return Arr.is(value) && value.length >= 2 && value.every(Str.is)
+  return Arr.is(value) && value.length >= 1 && value.every(Str.is)
 }
 
 // argv
 
 export interface Argv {
   execPath: string
-  scriptPath: string
+  /**
+   * Not present for example when in a NodeJS REPL.
+   */
+  scriptPath: null | string
   args: string[]
 }
 
 export const parseArgvOrThrow = (value: unknown): Argv => {
   if (!isProcessArgvLoose(value)) throw new Error(`Invalid argv: ${value}`)
 
-  const [execPath, scriptPath, ...args] = value
+  const [execPath, scriptPath = null, ...args] = value
 
   return {
     execPath,
