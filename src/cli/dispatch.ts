6085594c0eb5a10683da17094e6e0a7a6e3964cd
Jason Kuhrt <jasonkuhrt@me.com>
2025-05-23 14:47:07 -0400

    fix(cli): bad import

--- src/cli/dispatch.ts
+++ src/cli/dispatch.ts
@@ -1,6 +1,6 @@
 import { Arr, Fs, Path, Str } from '@wollybeard/kit'
-import { parseArgvOrThrow } from '../../build/cli/argv.js'
 import { pipe } from '../fn/pipe.js'
+import { parseArgvOrThrow } from './argv.js'
 import { type CommandTarget, getCommandTarget } from './commend-target.js'
 
 export const dispatch = async (commandsDirPath: string) => {
