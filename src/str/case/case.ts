b9868c15ca04dcd1cda78ebfd8dd43b1dd4c8ed3
Jason Kuhrt <jasonkuhrt@me.com>
2025-05-26 18:03:03 -0400

    feat(str)!: Case.title conversts -/_ into spaces

--- src/str/case/case.ts
+++ src/str/case/case.ts
@@ -1,3 +1,7 @@
 export { camelCase as camel, kebabCase as kebab, pascalCase as pascal, snakeCase as snake } from 'es-toolkit'
 
-export const title = (str: string) => str.replace(/\b\w/g, l => l.toUpperCase())
+export const title = (str: string) => {
+  return str
+    .replaceAll(/[-_]/g, ' ')
+    .replace(/\b\w/g, l => l.toUpperCase())
+}
