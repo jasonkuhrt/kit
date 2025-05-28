f0d153c540c5da2112c90966d639fbf1e6caf834
Jason Kuhrt <jasonkuhrt@me.com>
2025-05-27 21:43:02 -0400

    feat(http): method union type

--- src/http/http.ts
+++ src/http/http.ts
@@ -1,5 +1,17 @@
 export * as Headers from './headers.js'
-export * as Method from './method.js'
 export * as MimeType from './mime-type.js'
 export * as Response from './response.js'
 export * from './status/index.js'
+
+export * as Method from './method.js'
+
+import type * as _Method from './method.js'
+
+export type Method =
+  | _Method.delete
+  | _Method.get
+  | _Method.head
+  | _Method.options
+  | _Method.patch
+  | _Method.post
+  | _Method.put
