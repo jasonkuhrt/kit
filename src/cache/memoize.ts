c235f8715a02a9550894722add79e1dc2fa2d753
Jason Kuhrt <jasonkuhrt@me.com>
2025-06-02 18:06:39 -0400

    feat(cache): memoize optional key constructor

--- src/cache/memoize.ts
+++ src/cache/memoize.ts
@@ -1,9 +1,18 @@
-export const memoize = <func extends ((...args: any[]) => unknown)>(func: func): func => {
-  const cache: Record<string, unknown> = {}
+export const memoize = <fn extends ((...args: any[]) => unknown)>(
+  func: fn,
+  /**
+   * If `null` then key is by value reference.
 
-  return ((...args: unknown[]) => {
-    const cacheKey = JSON.stringify(args)
-    const cachedValue = cache[cacheKey]
+   * @default JSON.stringify
+   */
+  createKey?: null | ((parameters: Parameters<fn>) => unknown),
+): fn => {
+  const cache = new Map<unknown, unknown>()
+  const createKey_ = createKey === null ? null : createKey ? createKey : JSON.stringify
+
+  return ((...args: Parameters<fn>) => {
+    const cacheKey = createKey_?.(args) ?? args
+    const cachedValue = cache.get(cacheKey)
 
     if (cachedValue !== undefined) {
       return cachedValue
@@ -13,12 +22,12 @@ export const memoize = <func extends ((...args: any[]) => unknown)>(func: func):
 
     if (result instanceof Promise) {
       return result.then(resultResolved => {
-        cache[cacheKey] = resultResolved
+        cache.set(cacheKey, resultResolved)
         return resultResolved
       })
     }
 
-    cache[cacheKey] = result
+    cache.set(cacheKey, result)
     return result
-  }) as func
+  }) as fn
 }
